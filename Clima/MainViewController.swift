b9a0940ded88bc42b9db422f767cebc342a6f399
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2024-06-19 18:42:17 +0100

    setup ViewController

---
+++ Clima/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  Clima
+//
+//  Created by Sergey Zakurakin on 19/06/2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
