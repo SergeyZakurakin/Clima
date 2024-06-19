c116dd3e567b18a7a85d09ac5a336e7870c489ef
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2024-06-19 22:39:33 +0100

    finish UI

---
+++ Clima/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Clima
+//
+//  Created by Sergey Zakurakin on 19/06/2024.
+//
+
+import Foundation
+
+
+enum Constants {
+    static let background = "background"
+    static let weatherColor = "weatherColor"
+    static let celsius = "C"
+    static let searchSF = "magnifyingglass"
+    static let geoSF = "location.circle.fill"
+    static let search = "Search"
+    static let conditionSF = "cloud.rain"
+}
--- Clima/MainViewController.swift
+++ Clima/MainViewController.swift
@@ -6,12 +6,183 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
 
+    
+    private lazy var backgroundImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(named: Constants.background)
+        element.contentMode = .scaleAspectFill
+        
+        return element
+    }()
+    
+    private lazy var mainStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .vertical
+        element.spacing = 10
+        element.alignment = .trailing
+        
+        return element
+    }()
+    
+    private lazy var headerStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .horizontal
+        element.alignment = .fill
+        element.distribution = .fill
+        
+        return element
+    }()
+    
+    private lazy var geoButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setImage(UIImage(systemName: Constants.geoSF), for: .normal)
+        element.tintColor = .label
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var searchTextField: UITextField = {
+        let element = UITextField()
+        element.placeholder = Constants.search
+        element.borderStyle = .roundedRect
+        element.textAlignment = .right
+        element.font = .systemFont(ofSize: 25)
+        element.textColor = .label
+        element.tintColor = .label
+        element.backgroundColor = .systemFill
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var searchButton: UIButton = {
+        let element = UIButton(type: .system)
+        element.setImage(UIImage(systemName: Constants.searchSF), for: .normal)
+        element.tintColor = .label
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var conditionImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = UIImage(systemName: Constants.conditionSF)
+        element.tintColor = UIColor(named: Constants.weatherColor) // изправить
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var tempStackView: UIStackView = {
+        let element = UIStackView()
+        element.axis = .horizontal
+        
+        return element
+    }()
+    
+    private lazy var tempLabel: UILabel = {
+        let element = UILabel()
+        element.tintColor = .label
+        element.font = .systemFont(ofSize: 80, weight: .black)
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var tempTypeLabel: UILabel = {
+        let element = UILabel()
+        element.tintColor = .label
+        element.font = .systemFont(ofSize: 100, weight: .light)
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var cityLabel: UILabel = {
+        let element = UILabel()
+        element.tintColor = .label
+        element.font = .systemFont(ofSize: 30)
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    let emptyView = UIView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupView()
+        setupConstraints()
+        
+  
+    }
+    
+    private func setupView() {
+        view.addSubview(backgroundImageView)
+        view.addSubview(mainStackView)
+        
+        mainStackView.addArrangedSubview(headerStackView)
+        headerStackView.addArrangedSubview(geoButton)
+        headerStackView.addArrangedSubview(searchTextField)
+        headerStackView.addArrangedSubview(searchButton)
+        
+        mainStackView.addArrangedSubview(conditionImageView)
+        mainStackView.addArrangedSubview(tempStackView)
+        
+        tempStackView.addArrangedSubview(tempLabel)
+        tempStackView.addArrangedSubview(tempTypeLabel)
+        
+        mainStackView.addArrangedSubview(cityLabel)
+        mainStackView.addArrangedSubview(emptyView)
+        
+        tempLabel.text = "21"
+        tempTypeLabel.text = Constants.celsius
+        cityLabel.text = "London"
     }
 }
+
+extension MainViewController {
+    
+    private func setupConstraints() {
+        
+        backgroundImageView.snp.makeConstraints({ make in
+            make.edges.equalToSuperview()
+        })
+        mainStackView.snp.makeConstraints({ make in
+            make.edges.equalTo(view.safeAreaLayoutGuide).inset(24)
+        })
+        
+        headerStackView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+        }
+        
+        geoButton.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+        }
+        
+        searchButton.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+        }
+        
+        conditionImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(120)
+        }
+        
+        
+//        NSLayoutConstraint.activate([
+//            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+//            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//        ])
+        
+    }
+}
+
 
